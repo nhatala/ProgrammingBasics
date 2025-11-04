e66522286218067d943cf6ed72fffdd6bb776ada
Niko Hätälä <niko.hatala@centria.fi>
2025-11-05 00:20:59 +0200

    Added total price calculation

--- Viikko2/lue_varaukset.py
+++ Viikko2/lue_varaukset.py
@@ -7,46 +7,54 @@ def main():
 
 
     # Avataan tiedosto ja luetaan sisältö
-    with open(varaukset, "r", encoding="utf-8") as f:
-        for line in f:
-            varaus = line.strip().split("|")
+    with open(varaukset, "r", encoding="utf-8") as file:
+        for line in file:
+            varaus = line.split("|")
 
-    # Muutetaan tarvittavat tiedot oikeaan muotoon
-    paiva = datetime.strptime(varaus[2], "%Y-%m-%d").date()
-    suomalainenPaiva = paiva.strftime("%d.%m.%Y")
-    aika = datetime.strptime(varaus[3], "%H:%M").time()
-    suomalainenAika = aika.strftime("%H.%M")
-    varausnumero = int(varaus[0])
-    varaaja = str(varaus[1])
-    tuntimaara = int(varaus[4])
-    tuntihinta = round(float(varaus[5]), 2)
-    kokonaishinta = round(tuntimaara * tuntihinta, 2)   
-    maksettu = varaus[6] == "True"
-    kohde = str(varaus[7])
-    puhelin = str(varaus[8])
-    sahkoposti = str(varaus[9])
-    loppumispaiva = suomalainenPaiva if aika.hour + tuntimaara < 24 else (paiva.replace(day=paiva.day + 1)).strftime("%d.%m.%Y")
-    loppumisaika = f"{(aika.hour + tuntimaara) - 24}.{aika.minute:02d}" if aika.hour + tuntimaara >= 24 else f"{aika.hour + tuntimaara}.{aika.minute:02d}"
+            # Muutetaan tarvittavat tiedot oikeaan muotoon
+            paiva = datetime.strptime(varaus[2], "%Y-%m-%d").date()
+            suomalainenPaiva = paiva.strftime("%d.%m.%Y")
+            aika = datetime.strptime(varaus[3], "%H:%M").time()
+            suomalainenAika = aika.strftime("%H.%M")
+            varausnumero = int(varaus[0])
+            varaaja = str(varaus[1])
+            tuntimaara = int(varaus[4])
+            tuntihinta = round(float(varaus[5]), 2)
+            kokonaishinta = round(tuntimaara * tuntihinta, 2)
+            maksettu = varaus[6] == "True"
+            kohde = str(varaus[7])
+            puhelin = str(varaus[8])
+            sahkoposti = str(varaus[9])
+            loppumispaiva = suomalainenPaiva if aika.hour + tuntimaara < 24 else (paiva.replace(day=paiva.day + 1)).strftime("%d.%m.%Y")
+            loppumisaika = f"{(aika.hour + tuntimaara) - 24}.{aika.minute:02d}" if aika.hour + tuntimaara >= 24 else f"{aika.hour + tuntimaara}.{aika.minute:02d}"
 
-    # Muodostetaan varaustiedoista tulostettava merkkijono
-    varaustiedot = f"""
-    Varausnumero: {varausnumero}
-    Varaaja: {varaaja}
-    Päivämäärä: {suomalainenPaiva}
-    Aloitusaika: {suomalainenAika}
-    Loppumisaika: {loppumisaika}
-    Loppumispäivä: {loppumispaiva}
-    Tuntimäärä: {tuntimaara}
-    Tuntihinta: {str(tuntihinta).replace('.', ',')} €
-    Kokonaishinta: {str(kokonaishinta).replace('.', ',')} €
-    Maksettu: {"Kyllä" if maksettu else "Ei"}
-    Kohde: {kohde}
-    Puhelin: {puhelin}
-    Sähköposti: {sahkoposti}
-    """
+            # Muodostetaan varaustiedoista tulostettava merkkijono
+            varaustiedot = f"""
+            Varausnumero: {varausnumero}
+            Varaaja: {varaaja}
+            Päivämäärä: {suomalainenPaiva}
+            Aloitusaika: {suomalainenAika}
+            Loppumisaika: {loppumisaika}
+            Loppumispäivä: {loppumispaiva}
+            Tuntimäärä: {tuntimaara}
+            Tuntihinta: {str(tuntihinta).replace('.', ',')} €
+            Kokonaishinta: {str(kokonaishinta).replace('.', ',')} €
+            Maksettu: {"Kyllä" if maksettu else "Ei"}
+            Kohde: {kohde}
+            Puhelin: {puhelin}
+            Sähköposti: {sahkoposti}
+            """
 
-    # Tulostetaan varaustiedot
-    print(varaustiedot)
+            # Tulostetaan varaustiedot
+            print(varaustiedot)
+
+        yhteishinta = 0.0
+        with open(varaukset, "r", encoding="utf-8") as file:
+            for line in file:
+                yhteishinta += round(float(varaus[4]) * float(varaus[5]), 2)
+
+        print(f"Yhteishinta: {str(yhteishinta).replace('.', ',')} €")
+
 
 if __name__ == "__main__":
     main()
